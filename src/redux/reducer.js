139ca51d85e48b2eae4d8881398ee61446a925f7
Romana08 <mortonromina@gmail.com>
2022-01-17 18:58:42 -0300

    carpetas creadas

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+import {} from './actions';
+
+const inicialState = {
+    
+}; 
+const reducer = (state = inicialState, action) => {
+    switch (action.type) {
+
+    
+    default: return state 
+}
+}
+export default reducer; 
