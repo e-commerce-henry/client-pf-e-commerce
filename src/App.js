e784877a50c9d6c6a690468b3a80485c5b513a37
Jesus Cervantes <cervantes@ciencias.unam.mx>
2022-01-18 08:54:52 -0600

    Cambio en App Home to <Home />

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
       <div className="App">
         <Routes>
-          <Route path="/" element={ Home }/>
+          <Route path="/" element={ <Home/> }/>
           {/* <Route path="/" element={} />
           <Route path="/" element={} /> */}
         </Routes>
