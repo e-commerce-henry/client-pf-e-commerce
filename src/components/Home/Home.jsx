1d33639cbaff9b509b225650b721c96c018d1382
Romana08 <mortonromina@gmail.com>
2022-01-18 17:12:38 -0300

    MenuCategorias

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import Footer from "../Footer/Footer"
 import Head from "../Head/Head"
 import NavBar from "../Navbar/NavBar"
 import Cards from "../Cards/Cards"
+import Menu from "../menuCat/menu"
 
 export default function Home(){
     return(
@@ -9,6 +10,7 @@ export default function Home(){
         <p> del home</p>
         <Head />
         <NavBar />
+        <Menu />
         <Cards />
         <Footer />
         </>
