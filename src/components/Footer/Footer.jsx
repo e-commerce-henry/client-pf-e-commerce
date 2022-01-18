0e3ab9ddfef66adaab9425650b2807b5f43cffdf
Jesus Cervantes <cervantes@ciencias.unam.mx>
2022-01-18 10:53:36 -0600

    Diseño footer sin url

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,20 @@ import Style from './Footer.module.css'
 
 function Footer(){
     return(
-        <div className={Style.container} >
-            <ul>
-                <a href='#'>Sobre nosotros</a>
-            </ul>
-        </div>
+        <>
+            <div className={Style.container} >
+            
+                <div><a href='#'>Sobre nosotros</a></div>
+                <div><a href="#">Contacto</a></div>
+                <div><a href="#">terminos y condiciones</a></div>
+                <div><a href="#">Politica de privacidad</a></div>
+            
+            </div>
+            <div className={Style.copy}>
+                <p>&copy; E - commerce | 2022</p>
+            </div>
+        </>
+
     )
 };
 
