f7eab81d63464afa41a3bc2d316392e099978115
KimeyM <kimey.-@hotmail.com>
2022-01-21 20:10:15 -0300

    detalles en las cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -43,7 +43,7 @@ export default function Home(){
         <Pagination productsPerPage = {productsPerPage}
                     allProducts = {allProducts.length}
                     pagination = {pagination} />
-        {/* <Footer /> */}
+        <Footer />
         </>
     )
 };
