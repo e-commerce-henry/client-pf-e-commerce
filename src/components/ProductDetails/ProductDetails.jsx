207a8bca1c6329d5101f07a3712400857f5b52ac
Romana08 <mortonromina@gmail.com>
2022-01-22 12:21:47 -0300

    details stylo

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 import {productDetail} from '../../redux/actions'; 
+import { Link } from "react-router-dom";
 import Style from './ProductDetails.module.css'
 
 export default function ProductDetail(){
@@ -17,15 +18,22 @@ export default function ProductDetail(){
 
     return(
         <>
-            <div className={Style.container}>
+            <div className= {Style.cont}>
             <h3>{detailsProduct.name}</h3>
-            <img src= {detailsProduct.img} alt ="img"/>
+            <img src= {detailsProduct.img} alt ="img" width= "200px" />
             <h4><b>Marca: </b>{detailsProduct.brand} </h4>
-            <h4><b>Descripción: </b>{detailsProduct.description} </h4>
+            <h4 ><b>Descripción: </b>
+            <div className={Style.text}>{detailsProduct.description} </div></h4>
             <h4><b>Stock: </b>{detailsProduct.stock} </h4>
             <h4><b>Precio: </b>{detailsProduct.price} </h4>
-            </div>
+            <hr/>
+            <Link to={`/`}>
+                  <button className={Style.butt}>Volver</button> </Link>
+                  
+            </div> 
+            
         </>
+
     )
 };
 
