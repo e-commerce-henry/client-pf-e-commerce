1ec0aab8606e0b16bf40a87dd3673baa1459fdc0
LIDICES2021 <lidices.reyes@gmail.com>
2022-01-24 00:18:17 -0500

    arreglando sobreNosotros

--- src/components/SobreNosotros/SobreNosotros.jsx
+++ src/components/SobreNosotros/SobreNosotros.jsx
@@ -9,21 +9,19 @@ const SobreNosotros = () => {
   return (
   <div>
     <Head />
-    <div><h3 className="title1">SOMOS E-COMMERCE</h3></div>
+    <div><h3 className="title1">SOMOS MARCA DEL E-COMMERCE</h3></div>
     <div className="styleDiv"><p className="styleP">Somos una empresa de comercio electrónico online. 
     Nuestro principal capital invertido fue esfuerzo, trabajo y las ganas de superarnos día a día con la esperanza de crear una estructura sólida que nos permitiera hacer frente a los distintos desafíos que se nos podían presentar a lo largo del tiempo. 
     Nos enfocábamos principalmente en la venta de equipos de computo. Hemos ido incorporando distintos productos para su comercialización lo que nos llevó ampliar nuestra red online de ventas. 
     Apoyados en alianzas estratégicas con las principales marcas tecnológicas nos fuimos volviendo referentes del mercado, ampliando nuestra oferta a mas de 100 productos de última generación, los cuales mantenemos y renovamos día a día con el objetivo de trasladar a nuestros clientes la excelencia, calidad,innovación y vanguardia que nos exigimos en cada paso que damos.
-    Somos lo que querés, cuando querés, donde querés. Somos <b>E-COMMERCE.</b></p></div>
+    Somos lo que querés, cuando querés, donde querés. Somos <b>MARCA DEL E-COMMERCE.</b></p></div>
     <h3 className="title2">Nuestra Visión</h3>
     <div className="styleDiv"><p className="styleP1">Ser la empresa líder y de referencia, a nivel nacional, en la comercialización y distribución de equipos tecnológicos.</p></div>
     <h3 className="title3">Nuestra Misión</h3>
-    <div className="styleDiv"><p className="styleP2">Ofrecer productos tecnológicos que acompañen y satisfagan las necesidades de nuestros clientes, acompañando los mismos con la mejor atención, asesoramiento y servicio post venta. Brindando además, la posibilidad de que cualquier persona en todo el mundo pueda adquirir nuestros productos desde la comodidad de su hogar.</p></div>
-    <h1 className="contacto">Contacto</h1>
-    <div className="equipo"><Link to="/equipo">Nuestro Equipo</Link></div>
+    <p className="styleP2">Ofrecer productos tecnológicos que acompañen y satisfagan las necesidades de nuestros clientes, acompañando los mismos con la mejor atención, asesoramiento y servicio post venta. Brindando además, la posibilidad de que cualquier persona en todo el mundo pueda adquirir nuestros productos desde la comodidad de su hogar.</p>
+    {/* <div className="equipo"><Link to="/equipo">Nuestro Equipo</Link></div> */}
     <div className='nosfooter'><Footer /></div>
   </div>
   )
 }
-
 export default SobreNosotros;
