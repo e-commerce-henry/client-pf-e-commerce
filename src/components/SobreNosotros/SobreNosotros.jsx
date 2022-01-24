00810e37c148870e76f05256f4bd73fea4efc2d8
LIDICES2021 <lidices.reyes@gmail.com>
2022-01-24 13:20:49 -0500

    poniendo el div

--- src/components/SobreNosotros/SobreNosotros.jsx
+++ src/components/SobreNosotros/SobreNosotros.jsx
@@ -20,8 +20,7 @@ const SobreNosotros = () => {
     <h3 className="title3">Nuestra Misión</h3>
 
     <p className="styleP2">Ofrecer productos tecnológicos que acompañen y satisfagan las necesidades de nuestros clientes, acompañando los mismos con la mejor atención, asesoramiento y servicio post venta. Brindando además, la posibilidad de que cualquier persona en todo el mundo pueda adquirir nuestros productos desde la comodidad de su hogar.</p>
-   
-    <div className="styleDiv"><p className="styleP2">Ofrecer productos tecnológicos que acompañen y satisfagan las necesidades de nuestros clientes, acompañando los mismos con la mejor atención, asesoramiento y servicio post venta. Brindando además, la posibilidad de que cualquier persona en todo el mundo pueda adquirir nuestros productos desde la comodidad de su hogar.</p></div>
+  
     <div className='nosfooter'><Footer /></div>
   </div>
   )
