d48f8889dd0964608e24b421590b7b9b1b3ec40b
Kimey Mastrángelo <kimey.-@hotmail.com>
2022-01-27 14:01:50 -0300

    Update ShoppingCart.jsx

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import CartItem from "./CartItem";
-import { Link } from "react-router-dom";
 
 function valCart(e){
     if(e.id){
@@ -24,7 +23,7 @@ export default function ShoppingCart(){
     return(
         <>
             <div>
-                <Link to='/probando'> <h1>{`Este es tu carrito "Nombre del user"`}</h1></Link>
+                <h1>{`Este es tu carrito "Nombre del user"`}</h1>
                 <div>
                     {
                         
