b818c031b85e638f983dc3100fd4d69995dba637
KimeyM <kimey.-@hotmail.com>
2022-01-27 20:27:59 -0300

    anda el boton de eliminar de favs

--- src/components/Favs/CardFavs.jsx
+++ src/components/Favs/CardFavs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Style from './CardFavs.module.css';
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { addProductShoppingCart } from '../../redux/actions';
+import { addProductShoppingCart, deleteProductWishlist } from '../../redux/actions';
 
 function CardFavs({ id, name, price, img}){
     const dispatch = useDispatch()
@@ -11,6 +11,11 @@ function CardFavs({ id, name, price, img}){
         alert(`Agregado a carrito "${name}"`)
         dispatch(addProductShoppingCart(id))
     }
+
+    function deleteFavs(productId){
+        dispatch(deleteProductWishlist(productId))
+    }
+
     return(
         <div>
             
@@ -28,7 +33,7 @@ function CardFavs({ id, name, price, img}){
                             </svg>
                         </button>
 
-                        <button className={Style.productbtns}>
+                        <button className={Style.productbtns} onClick={() => deleteFavs(id)}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                             </svg>
