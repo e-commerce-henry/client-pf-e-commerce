b1debb647ffdb23571ecaecb75d9b2927f8fbf0b
LIDICES2021 <lidices.reyes@gmail.com>
2022-01-30 21:17:29 -0500

    redirigiendo cart a login

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import Head from '../Head/Head';
 import Style from './Cart.module.css'
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import { useSelector } from 'react-redux';
+import InicioSeccion from '../Profile/InicioSeccion';
 
 
 
@@ -15,7 +16,7 @@ function Cart(){
             <Head />
             <div className={Style.container} >
                 {
-                    auth?<ShoppingCart />: <p>Redirigir a logIn...</p>
+                    auth?<ShoppingCart />:  <InicioSeccion/>
                 }
                 
             </div>
