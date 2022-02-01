f85b6355a70ba67bf6c79a7970b928920889be9c
Romana08 <mortonromina@gmail.com>
2022-02-01 18:02:09 -0300

    eliminar

--- src/components/ShoppingCart/CartItem.jsx
+++ src/components/ShoppingCart/CartItem.jsx
@@ -1,12 +1,13 @@
 import React  from 'react';
 import Style from "./CartItems.module.css";
 import { Link } from "react-router-dom";
-import { editShoppingCart , getShoppingCart} from '../../redux/actions';
+import { editShoppingCart , getShoppingCart, removeCart, } from '../../redux/actions';
 /* import  removeCart  from '../../redux/actions'; */
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import swal from 'sweetalert'
 // import { useState } from 'react';
 
 
@@ -15,6 +16,7 @@ export default function CartItem({id, price, quantity, productId, addInfo}){
     const {name, img, brand, stock} = addInfo
     const dispatch = useDispatch()
     const userId = useSelector(state => state.idUser)
+     
     useEffect(() => {
         dispatch(getShoppingCart(userId));
     }, [dispatch]);
@@ -39,9 +41,18 @@ export default function CartItem({id, price, quantity, productId, addInfo}){
    }
 
 
-    function removeCart(productId){
-    dispatch(removeCart(productId))
-    }
+   async function deleteCart(productId){
+       
+     await dispatch(removeCart({productId, userId}))
+    swal({
+        title: "Se ha eliminado este producto del carrito:",
+        text: `${name}`,
+        icon: "warning",
+        button: "Ok"})
+        await dispatch(getShoppingCart(userId))
+
+    // window.location.reload("/favs");
+}
 
    
     return(
@@ -70,14 +81,14 @@ export default function CartItem({id, price, quantity, productId, addInfo}){
                 Precio por Unidad:<b> $ {price}</b> <br/>
                 SubTotal por {quantity} Unidad(es): $ {price * quantity}
                 <br/></div>
-{/*                  <button className={Style.boo} onClick={() => removeCart(productId)}>  
+                 <button className={Style.boo} onClick={() => deleteCart(productId)}>  
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                 </svg>
-                </button>  */}
+                </button> 
               
                 
-                {/* <div className='text'><Link to={`/products/${id}`}><img src={img} alt='not found' width="130px" height="100px"/></Link></div> */}
+                <div className='text'><Link to={`/products/${id}`}><img src={img} alt='not found' width="130px" height="100px"/></Link></div>
             </div>
         </div>
     )
