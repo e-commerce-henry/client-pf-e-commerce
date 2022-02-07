d9f64615db12a8bc874184c01a20855783f56a9e
KimeyM <kimey.-@hotmail.com>
2022-02-07 12:26:11 -0300

    listo los componentes post compra

--- src/components/CompraFallida/CompraFallida.jsx
+++ src/components/CompraFallida/CompraFallida.jsx
@@ -3,6 +3,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import Head from "../Head/Head";
 import { useEffect } from "react";
+import Footer from "../Footer/Footer";
+import '../CompraRealizada/CompraRealizada.css'
+
 
 export default function CompraFallida() {
   const navigate = useNavigate();
@@ -14,16 +17,20 @@ export default function CompraFallida() {
   },[])
   
   return (
-    <>
+    <div className="fadeinbck31">
       <Head/>
-      <div >
-        <h2>Lo sentimos mucho!</h2>
-        <h3>Algo ha salido mal con tu compra - Intentalo nuevamente</h3>
+      <div className="todoesto">
+        <h2 className="sorry">Lo sentimos mucho!</h2>
+        <div className="fail">
+          <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+          </svg>
+        </div>
+        <h3 className="procesando">Algo ha salido mal con tu compra - Intentalo nuevamente</h3>
       </div>
-      <div >
-        
-        Seras redirigido a la tienda en unos instantes...
+      <div className="redirigido">
+        Serás redirigido a la tienda en unos instantes...
       </div>
-    </>
+    </div>
   );
 }
--- src/components/CompraRealizada/CompraRealizada.jsx
+++ src/components/CompraRealizada/CompraRealizada.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import Head from "../Head/Head";
 import { useEffect } from "react";
+import Footer from "../Footer/Footer";
+import './CompraRealizada.css'
 
 export default function CompraRealizada() {
   const navigate = useNavigate();
@@ -13,20 +15,20 @@ export default function CompraRealizada() {
   },[])
   
   return (
-    <>
+    <div className="fadeinbck31">
       <Head/>
-      <div >
-        <h2>FELICITACIONES!</h2>
-        <h3>Ya estamos procesando tu pedido</h3>
+      <div className="todoesto">
+        <h2 className="felicitaciones">FELICITACIONES!</h2>
+        <div className="check">
+          <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+          </svg>
+        </div>
+          <h3 className="procesando">Ya estamos procesando tu pedido</h3>
       </div>
-      <div >
-        {/* <Link to="/">
-          <Button size='large' variant='contained'>
-            HOME
-          </Button>
-        </Link> */}
-        Seras redirigido a la tienda en unos instantes...
+      <div className="redirigido">
+        Serás redirigido a la tienda en unos instantes...
       </div>
-    </>
+    </div>
   );
 }
